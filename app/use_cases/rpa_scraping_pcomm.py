a32b14ccde5907443baa38e4c27ebeb941d7382c
Vinícius Giovanni <viniciusgiovanni2003@gmail.com>
2026-07-30 20:23:02 -0300

    feat: estrutura node js

---
+++ app/use_cases/rpa_scraping_pcomm.py
@@ -0,0 +1,11 @@
+import os
+import pandas as pd
+
+from service.S7DA import RoutineS7DA
+from infrastructure.dataframe import DataFrameManager
+from infrastructure.client import PcommClient
+from config.settings import 
+
+class ExecutePcommExtractAcertos:
+
+    ...
